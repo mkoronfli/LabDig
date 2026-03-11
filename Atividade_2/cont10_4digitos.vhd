9a0d90aedd896946e11f91a52569c5c5c5f28a13
LucasAroldo <lucasizoldi1@gmail.com>
2026-03-11 20:08:11 -0300

    Update cont10_4digitos.vhd

--- Atividade_2/cont10_4digitos.vhd
+++ Atividade_2/cont10_4digitos.vhd
@@ -48,23 +48,26 @@ library ieee;
 use ieee.std_logic_1164.all;
 use ieee.numeric_std.all;
 
-
+-- Declaracao do contador de 4 digitos
 entity cont10_4digitos is
     port (
         clock   : in  std_logic;
         clear   : in  std_logic;
         enable  : in  std_logic;
         Q0      : out std_logic_vector(3 downto 0);
-		Q1      : out std_logic_vector(3 downto 0);
-		Q2      : out std_logic_vector(3 downto 0);
-		Q3      : out std_logic_vector(3 downto 0);
+	Q1      : out std_logic_vector(3 downto 0);
+	Q2      : out std_logic_vector(3 downto 0);
+	Q3      : out std_logic_vector(3 downto 0);
         RCO     : out std_logic
     );
 end entity cont10_4digitos;
 
+-- Declaracao da arquitetura do contador de 4 digitos
 architecture arch of cont10_4digitos is
+	-- Sinais internos
 	signal RCO_uni, RCO_dez, RCO_cen, RCO_mil, enable_dez, enable_cen, enable_mil : std_logic;
 	
+	-- Declaracao do componente contador simples
 	component cont10 is
 		port (
 			clock   : in  std_logic;
@@ -75,19 +78,21 @@ architecture arch of cont10_4digitos is
 		);
 		end component;
 begin
-	
+	-- Logica de habilitacao dos contadores
 	enable_dez <= enable and RCO_uni;
-	enable_cen <= enable_dez and RCO_dez;
-	enable_mil <= enable_cen and RCO_cen;
+	enable_cen <= enable_dez and RCO_dez; -- Contador habilitado quando o contador anterior estiver
+	enable_mil <= enable_cen and RCO_cen; -- ligado e todos os digitos anteriores estiverem em 9
 	
+	-- Contador referente ao digito da unidade
 	UNI: cont10 port map(
-	clock => clock,
+	clock => clock, -- Clock ligado ao clock global
 	clear => clear,
 	enable => enable,	
-	Q => Q0,
+	Q => Q0, -- Saida ligada ao digito menos significativo do contador de 4 digitos
 	RCO => RCO_uni
 	);
-
+	
+	-- Contador do digito da dezena
 	DEZ: cont10 port map(
 	clock => clock,
 	clear => clear,
@@ -96,6 +101,7 @@ begin
 	RCO => RCO_dez
 	);
 
+	-- Contador da centena
 	CEN: cont10 port map(
 	clock => clock,
 	clear => clear,
@@ -104,6 +110,7 @@ begin
 	RCO => RCO_cen
 	);
 
+	-- Contador do milhar
 	MIL: cont10 port map(
 	clock => clock,
 	clear => clear,
@@ -112,7 +119,6 @@ begin
 	RCO => RCO_mil
 	);
 
-	RCO <= RCO_mil and RCO_cen and RCO_dez and RCO_uni;
-
+	RCO <= RCO_mil and RCO_cen and RCO_dez and RCO_uni; -- RCO do contador de 4 digitos ligado quando todos os digitos estao em 9 
 
 end architecture;
