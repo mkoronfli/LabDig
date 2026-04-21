5eb200b1cea136d6634a5fd9ba719852123429cb
LucasAroldo <lucasizoldi1@gmail.com>
2026-04-21 19:43:46 -0300

    Refactor LFSR entity to include clock and reset
    Added clock and reset ports to the LFSR entity and modified the architecture to handle state updates and output.

--- Exp_07/gerador_pseudo_aleatorio.vhd
+++ Exp_07/gerador_pseudo_aleatorio.vhd
@@ -2,17 +2,25 @@ library ieee;
 use ieee.std_logic_1164.all;
 
 entity gerador_pseudo_aleatorio is
-    port is (
-        enable: in std_logic;
-	    lfsr_state : in std_logic_vector(2 downto 0) := "011"; -- maximo 15 períodos de clock
-	    lfsr_out : out std_logic_vector(2 downto 0)
+    port (
+        clock    : in  std_logic;
+        reset    : in  std_logic;
+        lfsr_out : out std_logic_vector(2 downto 0)
     );
 end entity;
 
 architecture gerador of gerador_pseudo_aleatorio is
-    signal xor_result;
-
+    signal s_lfsr : std_logic_vector(2 downto 0) := "011"; -- maximo 15 períodos de clock
+begin
+    process(clock, reset)
     begin
-        xor_result <= lfsr_state(0) XOR lfsr_state(2); -- state proibido = "000"
-        lfsr_out <= xor_result & lfsr_state(2 downto 1) when enable = '1';
+        if reset = '1' then
+            s_lfsr <= "011";
+        elsif rising_edge(clock) then
+            s_lfsr <= (s_lfsr(0) xor s_lfsr(2)) & s_lfsr(2 downto 1);
+        end if;
+    end process;
+
+-- state proibido = "000"
+    lfsr_out <= s_lfsr;
 end architecture;
