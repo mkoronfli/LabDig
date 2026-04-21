7aa78a51c7ee5444cce6233bf6734d8ff68c6b91
LucasAroldo <lucasizoldi1@gmail.com>
2026-04-21 19:32:00 -0300

    Refactor jogo_reacao entity and internal signals

--- Exp_07/jogo_reacao.vhd
+++ Exp_07/jogo_reacao.vhd
@@ -3,20 +3,21 @@ use ieee.std_logic_1164.all;
 
 entity jogo_reacao is
     port (
-        clock    : in  std_logic;
-        reset    : in  std_logic;
-        jogar    : in  std_logic;
-        resposta : in  std_logic;
-        display0 : out std_logic_vector(6 downto 0);
-        display1 : out std_logic_vector(6 downto 0);
-        display2 : out std_logic_vector(6 downto 0);
-        display3 : out std_logic_vector(6 downto 0);
-        ligado   : out std_logic;
-        pulso    : out std_logic;
-        estimulo : out std_logic;
-        erro     : out std_logic;
-        pronto   : out std_logic;
-		db_estado: out std_logic_vector(3 downto 0)
+        clock      : in  std_logic;
+        reset      : in  std_logic;
+        jogar      : in  std_logic;
+        resposta_1 : in  std_logic; 
+        resposta_2 : in  std_logic; 
+        display0   : out std_logic_vector(6 downto 0);
+        display1   : out std_logic_vector(6 downto 0);
+        display2   : out std_logic_vector(6 downto 0);
+        display3   : out std_logic_vector(6 downto 0);
+        ligado     : out std_logic;
+        pulso      : out std_logic;
+        estimulo   : out std_logic;
+        erro       : out std_logic;
+        pronto     : out std_logic;
+		db_estado  : out std_logic_vector(3 downto 0)
     );
 end entity jogo_reacao;
 
@@ -62,30 +63,58 @@ architecture estrutural of jogo_reacao is
     -- Sinais internos
     signal s_ligado   : std_logic;
     signal s_estimulo : std_logic;
-    signal s_pulso    : std_logic;
+    signal s_pulso_int: std_logic;
     signal s_erro     : std_logic;
     signal s_pronto   : std_logic;
     signal s_db_estado: std_logic_vector(3 downto 0);
 
-    signal s_disp0, s_disp1, s_disp2, s_disp3 : std_logic_vector(6 downto 0);
+    signal s_disp1_0, s_disp1_1, s_disp1_2, s_disp1_3 : std_logic_vector(6 downto 0);
     signal s_tempo_1                          : std_logic_vector(15 downto 0);
     signal s_tempo_2                          : std_logic_vector(15 downto 0);
     signal s_win                              : std_logic;
+    
+    signal s_resposta_interface : std_logic;
+    signal s_resp_1_reg         : std_logic;
+    signal s_resp_2_reg         : std_logic;
+    signal s_pulso_1            : std_logic;
+    signal s_pulso_2            : std_logic;
 
 begin
+
+    process(clock, reset)
+    begin
+        if reset = '1' then
+            s_resp_1_reg <= '0';
+            s_resp_2_reg <= '0';
+        elsif rising_edge(clock) then
+            if s_db_estado = "0001" or s_db_estado = "1111" then 
+                s_resp_1_reg <= '0';
+                s_resp_2_reg <= '0';
+            else
+                if resposta_1 = '1' then s_resp_1_reg <= '1'; end if;
+                if resposta_2 = '1' then s_resp_2_reg <= '1'; end if;
+            end if;
+        end if;
+    end process;
+
+    s_resposta_interface <= (resposta_1 or resposta_2) when s_db_estado = "0010" else 
+                            (s_resp_1_reg and s_resp_2_reg);
+
+    s_pulso_1 <= s_estimulo and not s_resp_1_reg;
+    s_pulso_2 <= s_estimulo and not s_resp_2_reg;
 
     INTERFACE : interface_leds_botoes
         port map (
             clock      => clock,
             reset      => reset,
             iniciar    => jogar,     
-            resposta   => resposta,
+            resposta   => s_resposta_interface,
             ligado     => s_ligado,
             estimulo   => s_estimulo,
-            pulso      => s_pulso,
+            pulso      => s_pulso_int,
             erro       => s_erro,
             pronto     => s_pronto,
-            db_estado  => s_db_estado,       
+            db_estado  => s_db_estado,  
             db_rco     => open
         );
 
@@ -94,12 +123,12 @@ begin
             clock        => clock,
             reset        => reset,
             liga         => s_ligado, 
-            sinal        => s_pulso,  
+            sinal        => s_pulso_1,  
             tempo        => s_tempo_1,     
-            display0     => open,
-            display1     => open,
-            display2     => open,
-            display3     => open,
+            display0     => s_disp1_0,
+            display1     => s_disp1_1,
+            display2     => s_disp1_2,
+            display3     => s_disp1_3,
             db_estado    => open,
             pronto       => open,
             fim          => open,
@@ -114,7 +143,7 @@ begin
             clock        => clock,
             reset        => reset,
             liga         => s_ligado, 
-            sinal        => s_pulso,  
+            sinal        => s_pulso_2,  
             tempo        => s_tempo_2,     
             display0     => open,
             display1     => open,
@@ -129,22 +158,23 @@ begin
             db_contaCont => open
         );
 
-    display0 <= "0010000" when s_erro = '1' else s_disp0;
-    display1 <= "0010000" when s_erro = '1' else s_disp1;
-    display2 <= "0010000" when s_erro = '1' else s_disp2;
-    display3 <= "0010000" when s_erro = '1' else s_disp3;
+    s_win <= '0' when s_tempo_1 <= s_tempo_2 else '1';
 
-    s_win <= '0' when ( s_db_estado = "1000" AND s_tempo_1 < s_tempo_2) else
-             '1' when ( s_db_estado = "1000" AND s_tempo_1 > s_tempo_2);
+    display3 <= "1010101" when s_db_estado = "1000" else
+                "0010000" when s_erro = '1' else s_disp1_3;
 
-    display3 <=  "1010101" when s_db_estado = "1000";
-    display2 <=  "1111001" when s_db_estado = "1000";
-    display1 <=  "1001000" when s_db_estado = "1000";
-    display 0 <= "1111001" when (s_db_estado = "1000" and s_win = '0') else
-                 "0100100" when (s_db_estado = "1000" and s_win = '1');
+    display2 <= "1111001" when s_db_estado = "1000" else
+                "0010000" when s_erro = '1' else s_disp1_2;
+
+    display1 <= "1001000" when s_db_estado = "1000" else
+                "0010000" when s_erro = '1' else s_disp1_1;
+
+    display0 <= "1111001" when (s_db_estado = "1000" and s_win = '0') else
+                "0100100" when (s_db_estado = "1000" and s_win = '1') else
+                "0010000" when s_erro = '1' else s_disp1_0;
 
     ligado   <= s_ligado;
-    pulso    <= s_pulso;
+    pulso    <= s_pulso_1 or s_pulso_2;
     estimulo <= s_estimulo;
     erro     <= s_erro;
     pronto   <= s_pronto;
