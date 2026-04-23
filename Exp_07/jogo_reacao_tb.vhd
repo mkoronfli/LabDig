cc938eddc7562ee08a06d1348a7117f67a2015f8
LucasAroldo <lucasizoldi1@gmail.com>
2026-04-22 21:54:49 -0300

    Modify jogo_reacao_tb for two-player support
    Adapted the testbench for two players by adding a second response input and a new display for the winner. Updated the stimulus process to handle the new player interactions.

--- Exp_07/jogo_reacao_tb.vhd
+++ Exp_07/jogo_reacao_tb.vhd
@@ -8,41 +8,45 @@ end entity jogo_reacao_tb;
 architecture sim of jogo_reacao_tb is
 
     component jogo_reacao is
-    port (
-        clock    : in  std_logic;
-        reset    : in  std_logic;
-        jogar    : in  std_logic;
-        resposta : in  std_logic;
-        display0 : out std_logic_vector(6 downto 0);
-        display1 : out std_logic_vector(6 downto 0);
-        display2 : out std_logic_vector(6 downto 0);
-        display3 : out std_logic_vector(6 downto 0);
-        ligado   : out std_logic;
-        pulso    : out std_logic;
-        estimulo : out std_logic;
-        erro     : out std_logic;
-        pronto   : out std_logic;
-		db_estado: out std_logic_vector(3 downto 0)
-    );
+        port (
+            clock      : in  std_logic;
+            reset      : in  std_logic;
+            jogar      : in  std_logic;
+            resposta_1 : in  std_logic; -- Adaptado para 2 jogadores
+            resposta_2 : in  std_logic; -- Adaptado para 2 jogadores
+            display0   : out std_logic_vector(6 downto 0);
+            display1   : out std_logic_vector(6 downto 0);
+            display2   : out std_logic_vector(6 downto 0);
+            display3   : out std_logic_vector(6 downto 0);
+            display5   : out std_logic_vector(6 downto 0); -- Novo display do vencedor
+            ligado     : out std_logic;
+            pulso      : out std_logic;
+            estimulo   : out std_logic;
+            erro       : out std_logic;
+            pronto     : out std_logic;
+            db_estado  : out std_logic_vector(3 downto 0)
+        );
     end component;
 
     -- Sinais internos do testbench
     signal clock      : std_logic := '0';
     signal reset      : std_logic := '0';
-    signal jogar    : std_logic := '0';
-    signal resposta   : std_logic := '0';
+    signal jogar      : std_logic := '0';
+    signal resposta_1 : std_logic := '0';
+    signal resposta_2 : std_logic := '0';
     
     signal ligado     : std_logic;
     signal pulso      : std_logic;
     signal estimulo   : std_logic;
     signal erro       : std_logic;
     signal pronto     : std_logic;
-    signal db_estado : std_logic_vector(3 downto 0);
+    signal db_estado  : std_logic_vector(3 downto 0);
 
     signal display0 : std_logic_vector(6 downto 0);
     signal display1 : std_logic_vector(6 downto 0);
     signal display2 : std_logic_vector(6 downto 0);
     signal display3 : std_logic_vector(6 downto 0);
+    signal display5 : std_logic_vector(6 downto 0);
 
     signal keep_simulating : std_logic := '0';
     constant clockPeriod : time := 1 ms;
@@ -51,18 +55,18 @@ begin
 
     clock <= (not clock) and keep_simulating after clockPeriod/2;
 
-
-    -- Instanciacao do componente
     DUT: jogo_reacao
         port map (
             clock      => clock,
             reset      => reset,
             jogar      => jogar,
-            resposta   => resposta,
+            resposta_1 => resposta_1,
+            resposta_2 => resposta_2,
             display0   => display0,
             display1   => display1,
             display2   => display2,
             display3   => display3,
+            display5   => display5,
             ligado     => ligado,
             estimulo   => estimulo,
             pulso      => pulso,
@@ -71,7 +75,6 @@ begin
             db_estado  => db_estado 
         );
 
-    -- Processo de estimulos
     stim_process : process
     begin
         keep_simulating <= '1';
@@ -79,47 +82,55 @@ begin
         -- CASO 0: INICIALIZACAO E RESET
         reset <= '1';
         jogar <= '0';
-        resposta <= '0';
+        resposta_1 <= '0';
+        resposta_2 <= '0';
         wait for clockPeriod*2;
         reset <= '0';
         wait for clockPeriod;
 
-        -- CASO 1: JOGADA CORRETA 
+        -- CASO 1: JOGADA CORRETA (J1 ganha de J2)
         jogar <= '1';
         wait for clockPeriod;
         jogar <= '0';
 
-        wait for clockPeriod*5000; 
-        -- espera-se que o sinal de estímulo ligue (apos 5 segundos)
+        wait until estimulo = '1';
 
-        wait for clockPeriod*800; -- tempo de reacao
-        resposta <= '1';
-        wait for clockPeriod*100;
-        resposta <= '0';
+        -- Tempo de reacao do J1: 400 ms
+        wait for clockPeriod*400; 
+        resposta_1 <= '1';
+        wait for clockPeriod*10;
+        resposta_1 <= '0';
 
-        wait for clockPeriod*3;
-        -- saidas esperadas: erro = '0' e pronto = '1' o tamanho do pulso deve ser de 800
+        -- Tempo de reacao do J2: 600 ms
+        wait for clockPeriod*190;
+        resposta_2 <= '1';
+        wait for clockPeriod*10;
+        resposta_2 <= '0';
+        
+        wait for clockPeriod*10;
+        -- Saidas esperadas: erro = '0', pronto = '1', display5 deve mostrar '1' e display0-3 mostram "0400"
 
-        -- CASO 2: JOGADA INVALIDA (ERRO) 
+        reset <= '1';
+        wait for clockPeriod*2;
+        reset <= '0';
+        wait for clockPeriod;
+
+        -- CASO 2: JOGADA INVALIDA (ERRO DO JOGADOR 2) 
         jogar <= '1';
         wait for clockPeriod;
         jogar <= '0';
         
-
-        wait for clockPeriod*3000; 
-        -- contagem do tempo de espera nao termina
+        wait for clockPeriod*500; 
         
-        resposta <= '1';
-        wait for clockPeriod*100;
-        resposta <= '0';
+        resposta_2 <= '1'; 
+        wait for clockPeriod*10;
+        resposta_2 <= '0';
         
-        wait for clockPeriod*3;
-        -- saidas esperadas: erro = '1', pronto = '0', pulso sempre em '0'
+        wait for clockPeriod*10;
+        -- Saidas esperadas: erro = '1', pronto = '0', pulso sempre em '0', displays mostram 9999 e display5 apaga
 
         -- Fim da simulacao
-
         keep_simulating <= '0';
-
         wait;
     end process;
 
