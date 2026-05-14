2234a1661522afbf0f3b7596d36e36288161b695
LucasAroldo <lucasizoldi1@gmail.com>
2026-05-14 11:46:16 -0300

    Update tb_gerador_de_frutas.vhd

--- Projeto Final/tb_gerador_de_frutas.vhd
+++ Projeto Final/tb_gerador_de_frutas.vhd
@@ -9,24 +9,26 @@ architecture sim of tb_gerador_de_frutas is
 
     component gerador_de_frutas is
         port (
-            clock      : in  std_logic;
-            reset      : in  std_logic;
-            posicao_x  : in  std_logic_vector(6 downto 0);
-            posicao_y  : in  std_logic_vector(5 downto 0);
-            fruta_x    : out std_logic_vector(6 downto 0);
-            fruta_y    : out std_logic_vector(5 downto 0);
-            db_estado  : out std_logic_vector(1 downto 0)
+            clock        : in  std_logic;
+            reset        : in  std_logic;
+            iniciar_jogo : in  std_logic; 
+            posicao_x    : in  std_logic_vector(6 downto 0);
+            posicao_y    : in  std_logic_vector(5 downto 0);
+            fruta_x      : out std_logic_vector(6 downto 0);
+            fruta_y      : out std_logic_vector(5 downto 0);
+            db_estado    : out std_logic_vector(1 downto 0)
         );
     end component;
 
-    signal clock      : std_logic := '0';
-    signal reset      : std_logic := '0';
-    signal posicao_x  : std_logic_vector(6 downto 0) := (others => '0');
-    signal posicao_y  : std_logic_vector(5 downto 0) := (others => '0');
+    signal clock        : std_logic := '0';
+    signal reset        : std_logic := '0';
+    signal iniciar_jogo : std_logic := '0'; 
+    signal posicao_x    : std_logic_vector(6 downto 0) := (others => '0');
+    signal posicao_y    : std_logic_vector(5 downto 0) := (others => '0');
     
-    signal fruta_x    : std_logic_vector(6 downto 0);
-    signal fruta_y    : std_logic_vector(5 downto 0);
-    signal db_estado  : std_logic_vector(1 downto 0);
+    signal fruta_x      : std_logic_vector(6 downto 0);
+    signal fruta_y      : std_logic_vector(5 downto 0);
+    signal db_estado    : std_logic_vector(1 downto 0);
 
     signal keep_simulating : std_logic := '0';
     constant clockPeriod   : time := 20 ns;
@@ -37,13 +39,14 @@ begin
 
     DUT: gerador_de_frutas
         port map (
-            clock      => clock,
-            reset      => reset,
-            posicao_x  => posicao_x,
-            posicao_y  => posicao_y,
-            fruta_x    => fruta_x,
-            fruta_y    => fruta_y,
-            db_estado  => db_estado
+            clock        => clock,
+            reset        => reset,
+            iniciar_jogo => iniciar_jogo, 
+            posicao_x    => posicao_x,
+            posicao_y    => posicao_y,
+            fruta_x      => fruta_x,
+            fruta_y      => fruta_y,
+            db_estado    => db_estado
         );
 
     stim_process : process
@@ -52,13 +55,20 @@ begin
 
         -- CASO 0: INICIALIZACAO E RESET
         reset <= '1';
+        iniciar_jogo <= '0';
         posicao_x <= "0100000"; 
         posicao_y <= "010000";  
         wait for clockPeriod*2;
         reset <= '0';
+        wait for clockPeriod;
+
+        -- NOVO: PONTAPÃ‰ INICIAL PARA A PRIMEIRA FRUTA
+        iniciar_jogo <= '1';
+        wait for clockPeriod;
+        iniciar_jogo <= '0';
         wait for clockPeriod*5;
 
-        -- CASO 1: OBSERVAR A GERAÇÃO DE COORDENADAS
+        -- CASO 1: OBSERVAR A GERAÃ‡ÃƒO DE COORDENADAS
         wait for clockPeriod*20;
 
         -- CASO 2: SIMULAR MOVIMENTO DA COBRA
@@ -66,7 +76,7 @@ begin
         posicao_x <= "0100010"; wait for clockPeriod*2;
         posicao_y <= "010001";  wait for clockPeriod*10;
 
-        -- CASO 3: SIMULAR COLISÃO COM A FRUTA (COBRA COME A FRUTA)
+        -- CASO 3: SIMULAR COLISÃƒO COM A FRUTA (COBRA COME A FRUTA)
         posicao_x <= fruta_x;
         posicao_y <= fruta_y;
         wait for clockPeriod*5;
